b8b56c4aa54c145efea98f6479272ed82f2d8e3c
MMGISS <42643211+MMGISS@users.noreply.github.com>
2018-09-02 21:59:45 +0900

    Add files via upload

--- Hexapital/buildingData.js
+++ Hexapital/buildingData.js
@@ -1,9 +1,11 @@
+const version = "v1.3.201809021930";
+
 //images : 使用する画像数
 //pattern : 建造物の画像パターン
 //height : 建造物の高さ(0なら床パネル)
 //connectable : 道路が接続可能か
 //needsLifeLine : ライフラインが必要か
-//supplyable : ライフラインを供給できるか (1 : 自身への供給により供給可能, 2 : 単体で供給可能, 定義無し : 不可能)
+//supplyable : ライフラインを繋がれてから供給できるライフラインの数（定義なしなら一切供給不可）
 //LLlimit : ライフラインを伸ばせる長さ
 //icon : パレットで表示する画像名（定義が無けば画像"部品名"を表示）
 //buildArea : 建造物の全部品の相対座標（定義なしなら[[0,0]]で記録）
@@ -19,7 +21,7 @@ const buildingArr = {
         height:0,
         connectable:true,
         needsLifeLine:true,
-        supplyable:1,
+        supplyable:6,
         LLlimit:10
     },
     "r":{
@@ -59,8 +61,6 @@ const buildingArr = {
         images:1,
         pattern:[4],
         height:4,
-        connectable:true,
-        supplyable:2,
         icon:["air10","air10","air00"],
         buildArea:[[0,0],[-1,0],[-1,-1],[0,-1],[1,0],[0,1],[-1,1]]
     },
@@ -69,6 +69,6 @@ const buildingArr = {
         pattern:[3,1],
         height:4,
         connectable:true,
-        supplyable:2
+        supplyable:3
     }
 };
