5b8a9a3dc687ff7027ac10e0220db1f69dcb7d22
MMGISS <42643211+MMGISS@users.noreply.github.com>
2019-01-02 13:25:22 +0900

    Update src.js

--- Gasei-2018/src.js
+++ Gasei-2018/src.js
@@ -47,7 +47,8 @@ function draw() {
     if (nowDate.getTime() + debugTime > targetTime - 41300 && !played) {
         document.title = "ん？";
         setTimeout(()=>document.title = "流れ変わったな", 10000)
-        audio.currentTime = (nowDate.getTime() + debugTime - 1548946800000) / 1000 + 41.3;
+        audio.currentTime = (nowDate.getTime() + debugTime - targetTime) / 1000 - 413;
+
         audio.play();
         played = true;
     }
