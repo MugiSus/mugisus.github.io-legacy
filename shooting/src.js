590f667af8baca8bbfa15d6def99228e1aeaabd3
MMGISS <42643211+MMGISS@users.noreply.github.com>
2019-06-18 10:18:06 +0900

    Add files via upload

--- shooting/src.js
+++ shooting/src.js
@@ -25,8 +25,8 @@ resize();
 
 document.title = "Shooting";
 
-var stars = [], started = new Date().getTime(), time = 0, speed = 10, starDen = 0;
-while (stars.length < 100) stars.push([-900 + Math.random() * 1800, -1600 + Math.random() * 3200, 1 + Math.random() * 19, 0.5 + Math.random() * 0.5]);
+var stars = [], started = new Date().getTime(), time = 0, speed = 100, starDen = 0;
+while (stars.length < 60) stars.push([-900 + Math.random() * 1800, -1600 + Math.random() * 3200, 1 + Math.random() * 19, 0.5 + Math.random() * 0.5]);
 
 ctx.__proto__.line =(x0, y0, x1, y1)=> {
     ctx.beginPath();
@@ -45,14 +45,14 @@ var backGround =()=>{
     ctx.line(900,-1600,900,1600);
     ctx.line(900,1600,-900,1600);
     ctx.line(-900,1600,-900,-1600);
-    while (stars.length < 100) stars.push([-900 + Math.random() * 1800, -1600 + (Math.random() - 0.5) * speed, 1 + Math.random() * 19]);
+    while (stars.length < 60) stars.push([-900 + Math.random() * 1800, -1600 + (Math.random() - 0.5) * speed, 1 + Math.random() * 19]);
     stars.forEach((x,y)=>{
         ctx.globalAlpha = x[2] / 40;
         ctx.beginPath();
         ctx.arc(x[0], x[1], x[2], 0, Math.PI * 2, false);
         ctx.closePath();
         ctx.fill();
-        stars[y][1] += x[2] * speed / 20;
+        stars[y][1] += (x[2] ** 1.25) / (20 ** 1.25) * speed;
     });
     stars = stars.filter(x=>x[1] < 1600);
 }
@@ -61,9 +61,9 @@ function game(){
     time = new Date().getTime() - started;
     ctx.clearRect(canvas.width / -2 / ratio, canvas.height / -2 / ratio, canvas.width / ratio , canvas.height / ratio);
     if (time % 20000 < 5000) speed;
-    else if (time % 20000 < 10000) speed+=1;
+    else if (time % 20000 < 10000) speed+=0.5;
     else if (time % 20000 < 15000) speed;
-    else if (time % 20000 < 20000) speed-=1;
+    else if (time % 20000 < 20000) speed-=0.5;
     backGround();
     requestAnimationFrame(game);
 }
