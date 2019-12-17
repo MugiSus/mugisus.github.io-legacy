3bef5c71d30f5c5c11ef115a56cd80f204db8eaa
MMGISS <42643211+MMGISS@users.noreply.github.com>
2019-12-17 12:57:15 +0900

    Add files via upload

---
+++ trafficGirls/src.js
@@ -0,0 +1,6 @@
+document.title = "Traffic Girls";
+
+ctx.drawImage(img.tg001green, -750, -500);
+ctx.drawImage(img.tg001yellow, -250, -500);
+ctx.drawImage(img.tg001red, 250, -500);
+ctx.drawImage(img.tg001arrow, -750, 0);
