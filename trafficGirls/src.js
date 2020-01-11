17acb878d9e91218ca9c874400bd69159d57b76b
MMGISS <42643211+MMGISS@users.noreply.github.com>
2020-01-12 02:11:21 +0900

    rensa jissou commit

--- trafficGirls/src.js
+++ trafficGirls/src.js
@@ -1,6 +1,10 @@
 let width = 10; height = 18;
+let moving = false;
 let panels = [];
 let erasables = [];
+let erased = [];
+let markers = [];
+let comboLev = 5;
 let pathData = {
     roundSquere : (()=>{
         let path = new Path2D();
@@ -24,7 +28,7 @@ const panel = class {
 let process =(w, h)=>{
     let allMap = new Array(h).fill(0).map(()=>new Array(10).fill("empty"));
     let checkMap = new Array(h).fill(0).map(()=>new Array(10).fill("empty"));
-    let moving = false;
+    moving = false;
     allMap.push(new Array(w).fill("floor"));
     panels.forEach(i => allMap[Math.ceil(i.y)][i.x] = i.name);
     panels.forEach(i => {
@@ -56,6 +60,7 @@ let process =(w, h)=>{
 
 let drawBoard =(x, y, w, h)=> {
     let chipSize = Math.min(w / 10, h / 18);
+    ctx.globalAlpha = 1;
     ctx.lineWidth = chipSize * 0.25;
     ctx.lineJoin = "round";
     ctx.strokeStyle = "#343434";
@@ -66,7 +71,7 @@ let drawBoard =(x, y, w, h)=> {
             
             ctx.scale(chipSize / 200 * 0.85, chipSize / 200 * 0.85);
             ctx.stroke(pathData.roundSquere);
-        
+
             ctx.restore();
         }
     }
@@ -78,33 +83,55 @@ let drawBoard =(x, y, w, h)=> {
         ctx.globalAlpha = erasable ? 1 : 0.6;
         ctx.image(i.name, -0.475 * chipSize, -0.475 * chipSize, chipSize * 0.95, chipSize * 0.95);
         ctx.restore();
-        if (erasable) {
-            ctx.save();
-            ctx.translate(x + (i.x + 0.5) * chipSize, y + (i.y + 0.5) * chipSize);
-            ctx.scale(chipSize / 200 * 0.85, chipSize / 200 * 0.85);
-            ctx.strokeStyle = "#ffff44";
-            ctx.globalAlpha = Math.sin(new Date().getTime() % 1000 / 1000 * Math.PI * 2) * 0.3 + 0.7
-            ctx.stroke(pathData.roundSquere);
-            ctx.strokeStyle = "#343434";
-            ctx.restore();
+    });
+
+    ctx.strokeStyle = "#ffff44";
+    erasables.forEach(i => {
+        ctx.save();
+        ctx.translate(x + (i[0] + 0.5) * chipSize, y + (i[1] + 0.5) * chipSize);
+        ctx.scale(chipSize / 200 * 0.85, chipSize / 200 * 0.85);
+        ctx.globalAlpha = Math.sin(new Date().getTime() / 1000 * Math.PI * 2) * 0.3 + 0.7;
+        ctx.stroke(pathData.roundSquere);
+        ctx.restore();
+    });
+    
+    markers.forEach(i => {
+        i[2] += (0 - i[2]) / 10;
+        if (erased.some(x => x[0] == i[0] && x[1] == i[1])) {
+            ctx.globalAlpha = Math.min(Math.sin(new Date().getTime() / 1000 * Math.PI * 2) * 0.2 + 0.3 + i[2], 1);
+            ctx.fillStyle = "#ff6644";
+        } else {
+            ctx.globalAlpha = Math.min(Math.sin(new Date().getTime() / 1000 * Math.PI * 2) * 0.1 + 0.2 + i[2], 1);
+            ctx.fillStyle = "#ffff44";
         }
+        ctx.save();
+        ctx.translate(x + (i[0] + 0.5) * chipSize, y + (i[1] + 0.5) * chipSize);
+        ctx.scale(chipSize / 200 * 0.85, chipSize / 200 * 0.85);
+        ctx.fillRect(-112.5, -112.5, 225, 225);
+        ctx.restore();
     });
 }
 
-let erase =()=> {
+let markup =()=> {
+    erased.push(...erasables);
+    comboLev = markerPos.findIndex((x, y) => x[0] > erased.length || y == markerPos.length - 1) - 1;
+    markers = [];
+    erased.forEach(i => {
+        markerPos[comboLev].slice(1).forEach(j => {
+            if (!markers.some(x => x[0] == i[0] + j[0] && x[1] == i[1] + j[1]) && i[0] + j[0] < width && i[0] + j[0] >= 0 && i[1] + j[1] < height && i[1] + j[1] >= 0) markers.push([i[0] + j[0], i[1] + j[1], 1]);
+        })
+    })
     panels = panels.filter(i => !erasables.some(x=>x[0] == i.x && x[1] == i.y));
     erasables = [];
 }
 
-/*
-let checkTable = [
-    new table("tg001green", 0, 0, -1),
-    new table("tg001yellow", 1, 0, -1),
-    new table("tg001red", 2, 0, -1),
-    new table("tg001arrow", 0, 1, 0),
-];
-*/
+let erase =()=> {
+    panels = panels.filter(i => !markers.some(x=>x[0] == i.x && x[1] == i.y));
+    erased = [];
+    markers = [];
+}
 
+/*
 let checkTable = [
     new table("tg009yu-arrow", 0, 0, -1),
     new table("tg009yellow", 1, 0, -1),
@@ -113,8 +140,15 @@ let checkTable = [
     new table("tg009gl-arrow", 1, 1, 1),
     new table("tg009gr-arrow", 2, 1, 2),
 ];
+*/
 
-/*
+let checkTable = [
+    new table("tg001green", 0, 0, -1),
+    new table("tg001yellow", 1, 0, -1),
+    new table("tg001red", 2, 0, -1),
+    new table("tg001arrow", 0, 1, 0),
+];
+
 panels.push(new panel("tg001arrow", 3, 3));
 panels.push(new panel("tg001yellow", 4, 5));
 panels.push(new panel("tg001red", 5, 8));
@@ -143,14 +177,14 @@ panels.push(new panel("tg001green", 7, 0));
 panels.push(new panel("tg001red", 2, 13));
 panels.push(new panel("tg001yellow", 1, 1));
 panels.push(new panel("tg001green", 0, 2));
-*/
-
+/*
 
 for (let i = 0; i < height; i++) {
     for (let j = 0; j < width; j++) {
         if (Math.random() * checkTable.length > 1) panels.push(new panel(checkTable[Math.floor(Math.random()*checkTable.length)].name, j, i));
     }
 }
+*/
 
 function main(){
     ctx.clearRect(canvas.width / -2 / ratio, canvas.height / -2 / ratio, canvas.width / ratio , canvas.height / ratio);
@@ -158,5 +192,10 @@ function main(){
     drawBoard(-900, -900, 1800 * width / height, 1800);
     requestAnimationFrame(main);
 }
+
+canvas.addEventListener("click", ()=>{
+    if (erasables.length && !moving) markup();
+    else if (!moving) erase();
+})
 
 canvas.addEventListener("imageLoaded", ()=>main());
