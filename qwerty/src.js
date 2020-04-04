b5f8e54090d4d43405602fa9cd455b4ac14484fb
MMGISS <42643211+MMGISS@users.noreply.github.com>
2020-04-04 21:59:23 +0900

    added notes & lane

--- qwerty/src.js
+++ qwerty/src.js
@@ -1,3 +1,137 @@
+let pathPreset = {
+    diamond: (()=>{
+        let p = new Path2D();
+        p.moveTo(0, -100);
+        p.lineTo(100, 0);
+        p.lineTo(0, 100);
+        p.lineTo(-100, 0);
+        p.closePath();
+        return p;
+    })(),
+    q: (()=>{
+        let p = new Path2D();
+        p.moveTo(-50, -50);
+        p.lineTo(50, -50)
+        p.lineTo(50, 50);
+        p.lineTo(-50, 50)
+        p.closePath();
+        p.moveTo(10, 40);
+        p.lineTo(40, 10);
+        return p;
+    })(),
+    w: (()=>{
+        let p = new Path2D();
+        p.moveTo(-50, -50);
+        p.lineTo(-50, 50)
+        p.lineTo(50, 50);
+        p.lineTo(50, -50)
+        p.moveTo(0, 20);
+        p.lineTo(0, -50);
+        return p;
+    })(),
+    e: (()=>{
+        let p = new Path2D();
+        p.moveTo(50, -50);
+        p.lineTo(-50, -50)
+        p.lineTo(-50, 50);
+        p.lineTo(50, 50)
+        p.moveTo(-20, 0);
+        p.lineTo(50, 0);
+        return p;
+    })(),
+    r: (()=>{
+        let p = new Path2D();
+        p.moveTo(-50, 50);
+        p.lineTo(-50, -50);
+        p.lineTo(50, -50);
+        p.lineTo(50, 0);
+        p.lineTo(0, 0);
+        p.lineTo(50, 50);
+        return p;
+    })(),
+    t: (()=>{
+        let p = new Path2D();
+        p.moveTo(-50, -50);
+        p.lineTo(-50, 50); 
+        p.lineTo(50, 50);
+        p.moveTo(-20, -15);
+        p.lineTo(50, -15);
+        return p;
+    })(),
+    y: (()=>{
+        let p = new Path2D();
+        p.moveTo(50, -50);
+        p.lineTo(50, 50); 
+        p.lineTo(-50, 50);
+        p.moveTo(20, 0);
+        p.lineTo(-50, 0);
+        p.lineTo(-50, -50);
+        return p;
+    })(),
+    u: (()=>{
+        let p = new Path2D();
+        p.moveTo(-50, -50);
+        p.lineTo(-50, 50)
+        p.lineTo(50, 50);
+        p.lineTo(50, -50);
+        return p;
+    })(),
+    i: (()=>{
+        let p = new Path2D();
+        p.moveTo(-50, -50);
+        p.lineTo(0, -50)
+        p.lineTo(0, 50);
+        p.lineTo(50, 50);
+        p.moveTo(25, -50);
+        p.lineTo(50, -50);
+        p.moveTo(-25, 50);
+        p.lineTo(-50, 50);
+        return p;
+    })(),
+    o: (()=>{
+        let p = new Path2D();
+        p.moveTo(-50, -50);
+        p.lineTo(50, -50)
+        p.lineTo(50, 50);
+        p.lineTo(-50, 50)
+        p.closePath();
+        return p;
+    })(),
+    p: (()=>{
+        let p = new Path2D();
+        p.moveTo(-50, 50);
+        p.lineTo(-50, -50);
+        p.lineTo(50, -50);
+        p.lineTo(50, 0);
+        p.lineTo(-20, 0);
+        return p;
+    })(),
+    tapNote: (()=>{
+        let p = new Path2D();
+        p.moveTo(0, -100);
+        p.lineTo(100, 0);
+        p.lineTo(0, 100);
+        p.lineTo(-100, 0);
+        p.closePath();
+        p.moveTo(0, -50);
+        p.lineTo(0, 50);
+        p.moveTo(-50, 0);
+        p.lineTo(50, 0);
+        return p;
+    })(),
+    dragNote: (()=>{
+        let p = new Path2D();
+        p.moveTo(0, -100);
+        p.lineTo(100, 0);
+        p.lineTo(0, 100);
+        p.lineTo(-100, 0);
+        p.closePath();
+        p.moveTo(-50, 0);
+        p.lineTo(50, 0);
+        return p;
+    })(),
+}
+
 let getBezier =(x1, y1, x2, y2, x3, y3, x4, y4, t)=> [
     (1 - t) ** 3 * x1 + 3 * (1 - t) ** 2 * t * x2 + 3 * (1 - t) * t ** 2 * x3 + t ** 3 * x4,
     (1 - t) ** 3 * y1 + 3 * (1 - t) ** 2 * t * y2 + 3 * (1 - t) * t ** 2 * y3 + t ** 3 * y4
@@ -16,12 +150,11 @@ let getBezierYfromX =(x1, y1, x2, y2, x3, y3, x4, y4, px, trails = 16)=> {
     return (1 - t) ** 3 * y1 + 3 * (1 - t) ** 2 * t * y2 + 3 * (1 - t) * t ** 2 * y3 + t ** 3 * y4;
 }
 
-let getPosByPath =(pathStr, cons = false)=> {
+let getPosByPath =(pathStr)=> {
     let pos = [], lastPath = [], command = "";
     let path = pathStr.split(" ").map(x => x.split(",").map(x => x == x * 1 ? x * 1 : x));
 
     for (let i = 0; i < path.length; i++) {
-        if (cons) console.log(path[i]);
         if (path[i][0] * 1 != path[i][0]) command = path[i][0];
         else {
             if (lastPath.length) {
@@ -48,26 +181,42 @@ let getPosByPath =(pathStr, cons = false)=> {
 }
 
 let getPathFromX =(pos, px)=> {
-    let i;
+    let i = 0;
     for (i = 0; i < pos.length - 1; i++) if (pos[i + 1][0][0] > px) break;
+    console.log(i, pos)
     if (pos[i].length == 4) return getBezierYfromX(pos[i][0][0], pos[i][0][1], pos[i][1][0], pos[i][1][1], pos[i][2][0], pos[i][2][1], pos[i][3][0], pos[i][3][1], px);
     else return pos[i][0][1] + (pos[i][0][0] - px) / (pos[i][1][0] - pos[i][0][0]) * (pos[i][0][1] - pos[i][1][1]);
 }
 
+let drawqwerty =()=> {
+    ctx.strokeStyle = "#ffffff";
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 3;
+    ctx.save();
+    ctx.translate(-900, 0);
+    "qwertyuiop".split("").forEach(x => {
+        ctx.stroke(pathPreset.diamond);
+        ctx.stroke(pathPreset[x]);
+        ctx.translate(200, 0);
+    })
+    ctx.restore();
+}
+
 function main(){
     ctx.clearRect(canvas.width / -2 / ratio, canvas.height / -2 / ratio, canvas.width / ratio, canvas.height / ratio);
-    ctx.strokeRect(0,0,400,400);
-    ctx.beginPath();
-    ctx.arc(xpos - 100, getPathFromX(getPosByPath("M 0,0 C 53.500503,0 66.007358,133.8031 82.974512,267.10311 100.21221,402.52866 152.01765,451.85842 161.3192,201.37076 h 40.32979 v 140.95952 h 40.3298 V 40.274155 h 40.3298 V 281.91907 h 40.3298 V -80.548298 h 40.3298 V 241.64492 L 400,400 h 1"), xpos - 100), 20, 0, Math.PI * 2);
-    ctx.fill();
+    drawqwerty();
+    ctx.strokeStyle = "#88ffff";
+    ctx.fillStyle = "#88ffff44";
+    ctx.save();
+    ctx.translate(-100, getPathFromX(getPosByPath("m 0,0 12.896322,12.896322 c 3.718238,-15.6582264 7.825074,-6.8347719 12.092285,12.092285 3.316069,-37.978964 7.09452,-18.6226333 10.955404,10.955404 2.736437,-7.099773 5.170305,-19.41887 10.025228,10.025228 1.495276,-38.0570308 4.534564,-31.955339 8.784993,8.784993 2.14429,-10.528004 4.923381,-10.989884 8.991699,8.991699 4.221462,-35.61413 6.793797,-12.409364 9.405111,9.405111 3.330504,-15.373946 6.245326,-15.783355 8.474935,8.474935 C 84.233363,67.618533 84.987297,9.8233084 91.651205,91.651205 94.71882,85.324278 97.483476,88.692039 100,100 l 1,0.5"), xpos) * 9 - 900)
+    ctx.stroke(pathPreset.dragNote);
+    ctx.fill(pathPreset.diamond);
+    ctx.restore();
+    xpos += 0.5;
     requestAnimationFrame(main);
-    xpos += 1;
-    xpos %= 600;
 }
 
 let xpos = 0;
 
-ctx.strokeStyle = "#ffffff";
-ctx.lineWidth = 5;
-ctx.fillStyle = "#ff8800";
 main();
