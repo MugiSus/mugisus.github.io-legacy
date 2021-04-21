1a00bac489ac32cdd9d294e7d21965a646af84a7
MMGISS <mmgiss256@gmail.com>
2021-04-21 22:21:42 +0900

    Revert "interval testing"
    This reverts commit ce146b13ecf0793369e5521788ba8bbc4ab3bae5.

--- TELPort-txt-to-snd/src.js
+++ TELPort-txt-to-snd/src.js
@@ -1,55 +1,47 @@
 
 const frequency = new Array(16).fill(0).map((_, i) => {
-    return i < 6 ? 440 * 2 ** (i / 6) :
-           i < 18 ? 880 * 2 ** ((i - 6) / 12) :
-           i < 42 ? 1760 * 2 ** ((i - 18) / 24) :
-           0;
+    if (i < 12)
+        return 880 * 2 ** (i / 12);
+    if (i < 36)
+        return 1760 * 2 ** ((i - 12) / 24);
 });
 
 document.getElementById("text").value = localStorage["textToSound"] || "hello, world! ðøüþÿ";
 document.getElementById("sec").value = localStorage["soundSec"] || 0.5;
 
-document.getElementById("dispatcher").addEventListener("click", function() {
-    let textToSound = document.getElementById("text").value;
-    let soundSec = document.getElementById("sec").value * 1000;
+let context;
+
+function beep(hertz, start, len) {
     
+    let gainNode = new GainNode(context);
+    gainNode.connect(context.destination);
+    gainNode.gain.value = 0.25;
+    
+    let oscillatorNode = new OscillatorNode(context);
+    oscillatorNode.type = "sine";
+    oscillatorNode.frequency.value = hertz;
+    oscillatorNode.connect(gainNode);
+    oscillatorNode.start(context.currentTime + start);
+    oscillatorNode.stop(context.currentTime + start + len);
+}
+
+document.getElementById("dispatcher").addEventListener("click", function() {
+
+    context = new AudioContext();
+    
+    let textToSound = document.getElementById("text").value;
+    let soundSec = document.getElementById("sec").value;
+
     localStorage["textToSound"] = textToSound;
     localStorage["soundSec"] = soundSec;
     
     alert(`going to sound "${textToSound}" ${soundSec} sec per note`);
-    
     if (confirm("ready?")) {
-        let context = new AudioContext();
-
-        let beeps = new Array(frequency.length).fill(0).map(() => {
-            let gainNode = new GainNode(context);
-            gainNode.connect(context.destination);
-            gainNode.gain.value = 0.25;
-            
-            let oscillatorNode = new OscillatorNode(context);
-            oscillatorNode.frequency.value = 0;
-            oscillatorNode.type = "sine";
-            oscillatorNode.connect(gainNode);
-
-            oscillatorNode.start();
-
-            return oscillatorNode;
-        });
-
-        console.log(beeps);
-
-        let mainInterval, index = 0;
-
-        mainInterval = setInterval(() => {
-            frequency.forEach((hertz, bits) => {
-                if ((textToSound.codePointAt(index) >> bits) & 1) {
-                    beeps[bits].frequency.value = hertz;
-                    setTimeout(() => {beeps[bits].frequency.value = 0;}, soundSec * 0.8);
-                }
-            })
-
-            index++;
-            if (index > textToSound.length) clearInterval(mainInterval);
-        }, soundSec);
+        for (let i = 0; i < textToSound.length; i++) {
+            console.log(`attempting ${i}...`);
+            frequency.forEach((f, index) => {
+                if ((textToSound.codePointAt(i) >> index) & 1) beep(f, i * soundSec, soundSec * 0.8);
+            });
+        }
     }
 })
