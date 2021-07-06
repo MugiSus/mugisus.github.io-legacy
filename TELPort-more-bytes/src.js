6e094c05ae2827021e4825cdd33659fe6baebc5f
MugiSus <mmgiss256@gmail.com>
2021-07-06 11:46:17 +0900

    optionalized the visualizer
    optionalized(??)

--- TELPort-more-bytes/src.js
+++ TELPort-more-bytes/src.js
@@ -5,10 +5,14 @@ const frequency = new Array(8 * bytes).fill(0).map((_, i) => {
     return firstFreuency + 20 * i;
 });
 
-document.getElementById("text").value = localStorage["textToSound"] || "hello, world! ðøüþÿ";
-document.getElementById("sound-sec").value = localStorage["soundSec"] || 0.5;
-document.getElementById("fft-size").value = localStorage["fft-size"] || 13;
-document.getElementById("threshold").value = localStorage["threshold"] || 160;
+document.getElementById("text").value = localStorage["textToSound"] ?? "hello, world! ðøüþÿ";
+document.getElementById("sound-sec").value = localStorage["soundSec"] ?? 0.5;
+document.getElementById("visualize").checked = localStorage["visualize"] == "true";
+document.getElementById("fft-size").value = localStorage["fft-size"] ?? 13;
+document.getElementById("threshold").value = localStorage["threshold"] ?? 160;
+
+document.getElementById("sound-sec-number").value = localStorage["soundSec"] ?? 0.5;
+document.getElementById("threshold-number").value = localStorage["threshold"] ?? 160;
 
 document.getElementById("boxes").innerHTML = frequency.map((x, y) => `<div class="box"><span>${Math.round(x)}Hz<br>2<sup>${y}</sup></span></div>${(y + 1) % 16 ? "" : "<br>"}`).join(" ");
 let boxesHTMLCollection = document.getElementsByClassName("box");
@@ -24,14 +28,14 @@ const boxColorsCollection = {
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 let context;
 
-let soundText_intervalId;
+let visualize;
+let soundText_intervalId, textToSound, soundSec;
 let frequencyData, stream, input, analyser, threshold, fftSize, listenTextLoop_reqId, heardChars;
-let frameCounter = 0;
 
 function beep(hertz, start, len) {
     let gainNode = new GainNode(context);
     gainNode.connect(context.destination);
-    gainNode.gain.value = 0.007;
+    gainNode.gain.value = 0.005;
     
     let oscillatorNode = new OscillatorNode(context);
     oscillatorNode.type = "sine";
@@ -67,9 +71,10 @@ function soundText(textToSound, soundSec) {
 
         frequency.forEach((f, index) => {
             if ((textToSound.codePointAt(i + Math.floor(index / 8)) >> (index % 8)) & 1) {
-                boxesHTMLCollection[index].style.background = boxColorsCollection.green_sound;
+                if (visualize)
+                    boxesHTMLCollection[index].style.background = boxColorsCollection.green_sound;
                 beep(f, 0, soundSec * 0.4);
-            } else 
+            } else if (visualize) 
                 boxesHTMLCollection[index].style.background = boxColorsCollection.green_mute;
         });
 
@@ -79,13 +84,9 @@ function soundText(textToSound, soundSec) {
     }, soundSec * 1000);
 }
 
-let fps = 0;
-
 function listenTextLoop() {
     listenTextLoop_reqId = requestAnimationFrame(listenTextLoop);
     //listenTextLoop_reqId = setTimeout(listenTextLoop, 1000 / 120);
-
-    if (fps++ % 120 == 0) console.log("cycle");
 
     let codePoints = new Array(bytes).fill(0);
     let isMute = true;
@@ -94,10 +95,11 @@ function listenTextLoop() {
     
     frequency.forEach((f, index) => {
         if (threshold <= frequencyData[Math.floor(f / (context.sampleRate / analyser.fftSize))]) {
-            boxesHTMLCollection[index].style.background = boxColorsCollection.red_sound;
+            if (visualize)
+                boxesHTMLCollection[index].style.background = boxColorsCollection.red_sound;
             codePoints[Math.floor(index / 8)] += 2 ** (index % 8);
             isMute = false;
-        } else 
+        } else if (visualize)
             boxesHTMLCollection[index].style.background = boxColorsCollection.red_mute;
     });
     
@@ -135,11 +137,13 @@ function initallaize(){
 document.getElementById("call-button").addEventListener("click", function() {
     initallaize();
     
-    let textToSound = document.getElementById("text").value;
-    let soundSec = document.getElementById("sound-sec").value;
+    textToSound = document.getElementById("text").value;
+    soundSec = document.getElementById("sound-sec").value;
+    visualize = document.getElementById("visualize").checked;
 
     localStorage["textToSound"] = textToSound;
     localStorage["soundSec"] = soundSec;
+    localStorage["visualize"] = visualize;
     
     alert(`going to sound "${textToSound.length > 50 ? textToSound.substring(0, 100) + "..." : textToSound}" ${soundSec} sec per note`);
     
@@ -147,7 +151,7 @@ document.getElementById("call-button").addEventListener("click", function() {
         [...boxesHTMLCollection].forEach(x => x.style.background = boxColorsCollection.green_mute);
         soundText(textToSound, soundSec);
     }
-})
+});
 
 document.getElementById("rec-button").addEventListener("click", async() => {
     initallaize();
@@ -160,16 +164,41 @@ document.getElementById("rec-button").addEventListener("click", async() => {
     heardChars = new Array(bytes).fill(0).map(() => new Object());
     
     document.getElementById("received-text").value = "";
+
     threshold = document.getElementById("threshold").value;
     fftSize = document.getElementById("fft-size").value;
+    visualize = document.getElementById("visualize").checked;
 
-    localStorage["threshold"] = threshold;
     localStorage["fft-size"] = fftSize;
+    localStorage["visualize"] = visualize;
+
     analyser.fftSize = 2 ** fftSize;
     
     alert(`initalization succeded!\n>>>caution: work in progress<<<\n\nthreshold = ${threshold};\nanalyser.fftSize = ${analyser.fftSize};`);
     frequencyData = new Uint8Array(analyser.frequencyBinCount);
     timeDomainData = new Uint8Array(analyser.fftSize);
 
+    [...boxesHTMLCollection].forEach(x => x.style.background = boxColorsCollection.red_mute);
     listenTextLoop();
-})
+});
+
+document.getElementById("threshold").addEventListener("change", () => {
+    document.getElementById("threshold-number").value = document.getElementById("threshold").value;
+    threshold = document.getElementById("threshold").value;
+    localStorage["threshold"] = threshold;
+});
+
+document.getElementById("threshold-number").addEventListener("change", () => {
+    document.getElementById("threshold").value = document.getElementById("threshold-number").value;
+    threshold = document.getElementById("threshold").value;
+    localStorage["threshold"] = threshold;
+});
+
+document.getElementById("sound-sec").addEventListener("change", () => {
+    document.getElementById("sound-sec-number").value = document.getElementById("sound-sec").value;
+});
+
+document.getElementById("sound-sec-number").addEventListener("change", () => {
+    document.getElementById("sound-sec").value = document.getElementById("sound-sec-number").value;
+});
+
