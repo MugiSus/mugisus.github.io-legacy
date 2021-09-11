cf2061b92ee7e1b31d3f643179d3e5e32b3c747a
MugiSus <mmgiss256@gmail.com>
2021-09-12 02:29:04 +0900

    touch-action: manipulation

--- TELPort-app/scroll-canceler.js
+++ TELPort-app/scroll-canceler.js
@@ -1,7 +1,3 @@
 document.addEventListener("touchmove", event => {
     event.preventDefault();
 }, {passive: false});
-
-document.addEventListener("dblclick", event => {
-    event.preventDefault();
-}, {passive: false});
