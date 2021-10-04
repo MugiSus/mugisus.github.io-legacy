d9f6b9db03341c0be3c3baf3165649b6e2e67802
MugiSus <mmgiss256@gmail.com>
2021-10-05 01:42:04 +0900

    done scroll

--- TELPort-app/scroll-manager.js
+++ TELPort-app/scroll-manager.js
@@ -1,3 +1,11 @@
-document.addEventListener("touchmove", event => {
-    event.preventDefault();
-}, {passive: false});
+// document.addEventListener("touchmove", event => {
+//     event.preventDefault();
+// }, {passive: false});
+
+document.getElementById("startup-call-container").addEventListener("click", () => {
+    document.getElementById("window-call").scrollIntoView({behavior: "smooth"})
+});
+
+document.getElementById("startup-listen-container").addEventListener("click", () => {
+    document.getElementById("window-listen").scrollIntoView({behavior: "smooth"})
+});
