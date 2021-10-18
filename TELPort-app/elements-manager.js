d118e3961979c3a909045cb4f7421effdd3451e1
MugiSus <mmgiss256@gmail.com>
2021-10-18 13:42:08 +0900

    Update elements-manager.js

--- TELPort-app/elements-manager.js
+++ TELPort-app/elements-manager.js
@@ -142,10 +142,6 @@ let windowOrderArray = [...document.getElementById("container").children].map(el
 [...document.getElementsByClassName("interwindow-arrow")].forEach(element => {
     element.addEventListener("click", (event) => {
         let scrollIntoViewTarget = windowOrderArray[windowOrderArray.indexOf(event.target.parentElement.id) + (event.target.classList.contains("interwindow-arrow-right") ? 1 : -1)];
-        if (scrollIntoViewTarget == "window-startup") {
-            [...document.getElementsByClassName("cancelable-button-container")].forEach(element => element.classList.remove("clicked"));
-            initialize();
-        }
         document.getElementById(scrollIntoViewTarget).scrollIntoView({behavior: "smooth"});
     });
 })
