b70dd1f0c7236f7d4e3baa5d33de10fc9ff67523
MugiSus <mmgiss256@gmail.com>
2021-10-23 02:22:48 +0900

    Update src.js

--- TELPort-app/src.js
+++ TELPort-app/src.js
@@ -1,8 +1,8 @@
 const FFTsize = 4096;
 const FirstFreuency = (44100 / FFTsize) * 80;
-const BytesPerRound = 12;
+const BytesPerRound = 20;
 const Frequencies = new Array(8 * BytesPerRound).fill(0).map((_, i) => {
-    return FirstFreuency + (44100 / FFTsize * 9) * i;
+    return FirstFreuency + (44100 / FFTsize * 5) * i;
 });
 
 const TuningString = "Tuning completed. It is time to telport.".slice(0, BytesPerRound);
